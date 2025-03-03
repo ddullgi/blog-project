9270731de789e807a2d0b5411ec4864b9ecb30bd
황상윤 <s_infinite@naver.com>
2025-03-03 23:30:10 +0900

    [fix] font 오류 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,9 +20,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={`${pretendard.variable} font-pretendard`}>
-				{children}
-			</body>
+			<body className={pretendard.className}>{children}</body>
 		</html>
 	);
 }
